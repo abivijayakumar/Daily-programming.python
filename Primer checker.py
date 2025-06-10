026d49293c72dfb8ed0ed47eb7859df5eb8568ee
Abinaya <abivijayakumar419@gmail.com>
2025-06-10 10:22:40 +0530

    Create Primer checker.py

---
+++ Primer checker.py
@@ -0,0 +1,10 @@
+num = int(input("Number: "))
+if num < 2:
+    print("Not prime")
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print("Not prime")
+            break
+    else:
+        print("Prime")
