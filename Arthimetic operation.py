7a14b226b0929672a3c42070c6851ea99a3fec6f
Abinaya <abivijayakumar419@gmail.com>
2025-07-01 20:12:57 +0530

    Create Arthimetic operation.py

---
+++ Arthimetic operation.py
@@ -0,0 +1,10 @@
+a = 100
+b = 10
+print("The addition = " ,a+b)
+print("The Subtraction= ",a-b)
+print("The Multiplication= ",a*b)
+print("The division= ",a/b)
+print("The remainder =",a%30)
+print("The Exponent =",a **2)
+print("The Floor Division ="a//30)
+      
